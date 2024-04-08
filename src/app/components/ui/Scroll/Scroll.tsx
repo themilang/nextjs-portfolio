923c76a02d6b5422b324bc3419834d142b135763
Milan Ghimire <milanghimire@Milans-MacBook-Air.local>
2024-04-08 18:46:13 +0545

    redy code debugged

--- src/app/components/ui/Scroll/Scroll.tsx
+++ src/app/components/ui/Scroll/Scroll.tsx
@@ -7,13 +7,18 @@ const Scroll = () => {
     $(function() {
       $('a[href*="about"]').on('click', function(e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+        const targetHref = $(this).attr('href');
+        if (targetHref) { // Check if targetHref is not undefined
+          $('html, body').animate({ 
+            scrollTop: $(targetHref).offset()?.top ?? 0
+          }, 500, 'linear');
+        }
       });
     });
-  }, []); // Empty dependency array to ensure the effect runs only once
+  }, []);
 
   return (
-    <section className='demo  mt-68' id="section10" >
+    <section className='demo mt-68' id="section10" >
       <a href="#about"><span></span>Scroll</a>
     </section>
   );
